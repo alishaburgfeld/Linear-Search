a3161b2a0f423786cbeafa86f47edb7b269c7e84
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-24 16:26:15 -0400

    added JS File

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,35 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
-}
+//Linear Search::
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+// const arrayToSearchThrough = [];
+// for (let i = 1; i <= 1000; i++) {
+//     arrayToSearchThrough.push(i);
+// }
 
+// exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+//     for (let i=0;i<arrayToSearchThrough.length;i++) {
+//         if (arrayToSearchThrough[i]===valueToFind) {
+//             return i
+//         }
+//     }
+//     return undefined
+// };
+
+///Global Linear Search:
+
+let arr=["b","a","n","a","n","a","s"]
+
+const linearSearchGlobal = function(valueToFind, arr) {
+    let newArr=[]
+    for (let i=0;i<arr.length;i++) {
+        if (arr[i]===valueToFind) {
+            newArr.push(i)
+        }
+    }
+    if (newArr.length===0) {
+        return undefined
+    }
+    else {
+        return newArr
+    }
 };
-
+console.log(linearSearchGlobal("c",arr))
